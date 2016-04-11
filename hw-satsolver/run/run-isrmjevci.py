46b93dbb364025c1feecac4bf52ace4349d68443
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2016-04-11 09:46:17 +0200

    Another project and run script update

--- hw-satsolver/run/run-isrmjevci.py
+++ hw-satsolver/run/run-isrmjevci.py
@@ -3,6 +3,4 @@ import os
 import sys
 sys.path.insert(0, '../isrmjevci')
 import functions
-fileInput = sys.argv[1]
-functions.satSolver(fileInput)
-os.rename(fileInput.replace(".txt", "_solution.txt"), sys.argv[2])
+functions.satSolver(sys.argv[1], sys.argv[2])
