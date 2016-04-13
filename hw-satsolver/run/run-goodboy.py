ec709e1b7c11c512ade86c9dfeb60f2b5dc6709f
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2016-04-13 09:49:25 +0200

    Run script updates
    See bjur/SAT_solver#5 and mz3944/SAT-solver#2

--- hw-satsolver/run/run-goodboy.py
+++ hw-satsolver/run/run-goodboy.py
@@ -4,9 +4,9 @@ import sys
 sys.path.insert(0, '../goodboy')
 import SAT_Solver
 nrOfVars, exp = SAT_Solver.dataInput(sys.argv[1])
-satisfiable, solution = SAT_Solver.solveSAT(exp)
+satisfiable, solution = SAT_Solver.solveSAT(exp, nrOfVars)
 with open(sys.argv[2], 'w') as out:
     if satisfiable:
-        out.write(' '.join([int(x) for x in solution]))
+        out.write(' '.join([str(x) for x in solution]))
     else:
         out.write('Not satisfiable')
---
+++ hw-satsolver/run/run-sml.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ../sml/Solver/run.py $1 > $2
